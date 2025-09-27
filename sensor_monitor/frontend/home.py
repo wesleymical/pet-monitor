823b6a3e58d7d7642b9515d42b432e60767441b7
Wesley Mical <wesleymical@yahoo.com.br>
2025-09-27 19:09:53 -0300

    Update home.py

--- sensor_monitor/frontend/home.py
+++ sensor_monitor/frontend/home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Painel de Controle - Pet Monitor")
+st.title("Painel de Controle - Sensor Monitor")
 st.write("Bem-vindo ao painel de controle!")
 
 st.info("Use o menu lateral para acessar Monitor, Simulador, Dados e o Dashboard.")
